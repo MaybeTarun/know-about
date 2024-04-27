bcc3b97fc7db10a5096bba07e21d7181a45203e9
Tarun Gupta <tarunthegreat2003@gmail.com>
2024-04-27 11:08:52 +0530

    added myself

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ switch (command) {
     console.error('name not available');
     process.exit(1);
 }
+
+console.log('\n');
+console.log('\x1b[37mCreated by Tarun Gupta');
+console.log('\x1b[37mDM me at \x1b[38;5;39mhttps://twitter.com/messages/compose?recipient_id=maybetarun \x1b[37mto make ur own. \x1b[37m');
+console.log('\n');
--- tarun.js
+++ tarun.js
@@ -4,16 +4,13 @@ function tarun() {
     const link3 = 'http://www.linkedin.com/in/maybetarun';
     const link4 = 'https://twitter.com/maybetarun';
     const link5 = 'https://instagram.com/maybe._.tarun';
-    const links = '\n\x1b[37mMy Website:\x1b[0m \x1b[38;5;39mhttps://maybetarun.vercel.app/ \n\x1b[37mGitHub:\x1b[0m \x1b[38;5;39mhttps://github.com/maybetarun \n\x1b[37mLinkedIn:\x1b[0m \x1b[38;5;39mhttp://www.linkedin.com/in/maybetarun \n\x1b[37mTwitter(𝕏):\x1b[0m \x1b[38;5;39mhttps://twitter.com/maybetarun \n\x1b[37mInstagram:\x1b[0m \x1b[38;5;39mhttps://instagram.com/maybe._.tarun\x1b[37m';
+    const links = '\n\x1b[38;5;172mMy Website:\x1b[0m \x1b[38;5;39mhttps://maybetarun.vercel.app/ \n\x1b[38;5;172mGitHub:\x1b[0m \x1b[38;5;39mhttps://github.com/maybetarun \n\x1b[38;5;172mLinkedIn:\x1b[0m \x1b[38;5;39mhttp://www.linkedin.com/in/maybetarun \n\x1b[38;5;172mTwitter(𝕏):\x1b[0m \x1b[38;5;39mhttps://twitter.com/maybetarun \n\x1b[38;5;172mInstagram:\x1b[0m \x1b[38;5;39mhttps://instagram.com/maybe._.tarun\x1b[37m';
     console.log('\n');
-    console.log('Heyy!! I am \x1b[38;5;39mTarun Gupta\x1b[37m, also known by the alias \x1b[38;5;137mbread. \x1b[38;5;8myou ask why? even idk ;-;');
-    console.log('\x1b[37mI am an engineer. \x1b[38;5;8mI can do anything but engineering.');
-    console.log('\x1b[37mI like to code \x1b[32mr\x1b[34ma\x1b[35mn\x1b[36md\x1b[31mo\x1b[38;5;200mm \x1b[37mprograms, build \x1b[36mcool \x1b[37mlooking websites, \x1b[35mdesign \x1b[37mstuff and a lot of other \x1b[32mr\x1b[34ma\x1b[35mn\x1b[36md\x1b[31mo\x1b[38;5;200mm \x1b[37mahh things.');
+    console.log('\x1b[37mHeyy!! I am \x1b[38;5;41mTarun Gupta\x1b[37m, also known by the alias \x1b[38;5;172mbread. \x1b[38;5;247myou ask why? even idk ;-;');
+    console.log('\x1b[37mI am an engineer. \x1b[38;5;247mI can do anything but engineering.');
+    console.log('\x1b[37mI like to code \x1b[32mr\x1b[34ma\x1b[35mn\x1b[37md\x1b[31mo\x1b[38;5;200mm \x1b[37mprograms, build \x1b[37mcool \x1b[37mlooking websites, \x1b[35mdesign \x1b[37mstuff and a lot of other \x1b[32mr\x1b[34ma\x1b[35mn\x1b[37md\x1b[31mo\x1b[38;5;200mm \x1b[37mahh things.');
     console.log('Always looking forward to connect with people, learn new stuff and just grow.');
     console.log(links);
-    console.log('\n');
-    console.log('\x1b[38;5;8mCreated by Tarun Gupta');
-    console.log('\x1b[38;5;8mDM me at \x1b[38;5;39mhttps://twitter.com/MaybeTarun \x1b[38;5;8mto make ur own \x1b[37m');
-    console.log('\n');
 }
-module.exports = tarun;
+// module.exports = tarun;
+tarun();
