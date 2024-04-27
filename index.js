32f2858fd3d1cf5f84af14fbbf38de9fb34083aa
Tarun Gupta <tarunthegreat2003@gmail.com>
2024-04-27 10:53:08 +0530

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const tarun = require('tarun.js');
+
+const command = process.argv[2];
+
+switch (command) {
+  case 'tarun':
+    tarun();
+    break;
+  default:
+    console.error('name not available');
+    process.exit(1);
+}
